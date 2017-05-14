7bb0a6522a2a0861cdf516c26243f1b0faf515fe
Haviz Vaisal <havizvaisal@gmail.com>
2017-05-14 19:28:27 +0700

    Change account app name

--- insta_dj/instadj/settings.py
+++ insta_dj/instadj/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'rest_framework_swagger',
 
-    'account',
+    'profile',
     'posts'
 ]
 
--- insta_dj/profile/apps.py
+++ insta_dj/profile/apps.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class AccountConfig(AppConfig):
-    name = 'account'
+class ProfileConfig(AppConfig):
+    name = 'profile'
