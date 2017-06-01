4c4a143c1fa4f6d2e13c9133d7bfcacef1371c65
Haviz Vaisal <havizvaisal@gmail.com>
2017-06-01 14:23:10 +0700

    Add dummy models notification

--- insta_dj/notification/models.py
+++ insta_dj/notification/models.py
@@ -1,5 +1,17 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
-# Create your models here.
+
+class Notification(models.Model):
+    actor = models.ForeignKey(User, related_name='notification_actors')
+    target = models.ForeignKey(User, related_name='notifications')
+
+    content_type = models.ForeignKey(
+        ContentType, related_name='content_type_set_for_%(class)s')
+
+    object_pk = models.PositiveIntegerField('object ID')
+    object = GenericForeignKey(ct_field='content_type', fk_field='object_pk')
